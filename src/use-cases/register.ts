1242b6862fc92791ff1fdc812de610050a3e401a
Borges10002 <borges10002@gmail.com>
2023-12-24 14:36:53 -0400

    update

---
+++ src/use-cases/register.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
+import { hash } from "bcryptjs";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({ where: { email } });
+
+  if (userWithSameEmail) {
+    throw new Error("E-mail already exists.");
+  }
+
+  const prismaUsersRepository = new PrismaUsersRepository();
+
+  await prismaUsersRepository.create({ name, email, password_hash });
+}
