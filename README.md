5ce350db52261c71484968798e6a9b4311942a48
Borges10002 <borges10002@gmail.com>
2024-01-21 18:47:31 -0400

    test

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ GymPass style app.
 - [x] O usuário não deve poder se cadastrar com um e-mail duplicado;
 - [x] O usuário não deve fazer 2 check-ins no mesmo dia;
 - [x] O usuário não deve fazer check-in se não estiver perto (100m) da academia;
-- [ ] O ckeck-in só pode ser validado até 20 minutos após criado;
+- [x] O ckeck-in só pode ser validado até 20 minutos após criado;
 - [ ] O ckeck-in só pode ser validado por administradores;
 - [ ] A academia só pode ser cadastrado administradores;
 
--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,5 +1,5 @@
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
-import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { ValidateCheckInUseCase } from "./validate-check-in";
 
@@ -11,11 +11,11 @@ describe("Validade Check-in Use Case", () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInsRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    //  vi.useRealTimers();
+    vi.useRealTimers();
   });
 
   it("should be able to validade the check-in", async () => {
@@ -34,5 +34,24 @@ describe("Validade Check-in Use Case", () => {
     await expect(() =>
       sut.execute({ checkInId: "inexistent-check-in-id" })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("Should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(Error);
   });
 });
