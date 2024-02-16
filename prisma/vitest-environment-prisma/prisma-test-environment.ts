f6b8045f6bcd67608a2d9332c3a35c9b8d880179
Borges10002 <borges10002@gmail.com>
2024-02-15 21:50:39 -0400

    updae

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provid a DATABASE_URL environment variable.");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   transformMode: "web",
   async setup() {
-    console.log("Setup");
+    const shema = randomUUID();
+    const databaseURL = generateDatabaseURL(shema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync("npx prisma migrate deploy");
 
     return {
       async teardown() {
-        console.log("Teardown");
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${shema}" CASCADE`
+        );
+
+        await prisma.$disconnect();
       },
     };
   },
