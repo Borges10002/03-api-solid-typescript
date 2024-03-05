b4e47a1248436bf858573c7b5b2df19b45e1a881
Borges10002 <borges10002@gmail.com>
2024-03-04 21:00:24 -0400

    class about teste e2e do perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,4 +1,4 @@
-import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use.case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
@@ -8,5 +8,10 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
     userId: request.user.sub,
   });
 
-  return reply.status(200).send({ ...user, password_hash: undefined });
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
--- src/use-cases/factories/make-get-user-profile-use.case.ts
+++ src/use-cases/factories/make-get-user-profile-use.case.ts
@@ -2,8 +2,8 @@ import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-reposi
 import { GetUserProfileUseCase } from "../get-user-profile";
 
 export function makeGetUserProfileUseCase() {
-  const prismaUsersRepository = new PrismaUsersRepository();
-  const useCase = new GetUserProfileUseCase(prismaUsersRepository);
+  const usersRepository = new PrismaUsersRepository();
+  const useCase = new GetUserProfileUseCase(usersRepository);
 
   return useCase;
 }
